18e10f8ac5620eb3d024ac71ea7eaeff4b5baa6f
Harshmistry-197 <harshmistry072004@gmail.com>
2026-02-16 17:01:15 +0530

    Change : Implemented more advance method of beaautiful soup

--- 16022026-monday/Web Scraping/main.py
+++ 16022026-monday/Web Scraping/main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
 
 def basic_fetch():
@@ -56,10 +57,61 @@ def save_to_txt(soup):
             file.write(head.text+ "\n\n")
 
         for code in soup.select("div.highlight pre"):
-            file.write("----Code-------\n")
+            file.write("-----Code-------\n")
             file.write(code.text+"\n\n")
 
+def advance_scrape_section(soup):
+    sections = soup.find_all("div", class_="section")
 
+    for section in sections:
+        section_id = section.get("id")
+        title = section.find(["h1", "h2", "h3"])
+
+        if title:
+            print("SECTION:", title.get_text(strip=True))
+
+        print("-" * 50)
+
+
+
+
+def structure_data_extraction(soup):
+    data = []
+
+    sections = soup.find_all("div", class_="section")
+
+    for section in sections:
+        title_tag = section.find(["h1", "h2", "h3"])
+
+        if title_tag:
+            title = title_tag.get_text(strip=True)
+
+            content = section.get_text(strip=True)
+
+            data.append({
+                "title": title,
+                "content": content
+            })
+
+    print(data[:2])
+
+
+
+
+def sleep_program():
+    time.sleep(2)
+
+
+
+def advance_selector(soup):
+    # Select section by id
+    soup.select_one("#kinds-of-objects")
+
+    # Select all code blocks
+    soup.select("div.highlight > pre")
+
+    # Select nested heading
+    soup.select("div.section h2")
 
 
 if __name__ == "__main__":
@@ -70,4 +122,9 @@ if __name__ == "__main__":
     # extract_code_block(sup)
     scrape_all(sup)
     save_to_txt(sup)
+    sleep_program()
+    advance_scrape_section(sup)
+    structure_data_extraction(sup)
+    sleep_program()
+    advance_selector(sup)
     print("success")
