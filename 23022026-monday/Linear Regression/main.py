7033cf4e07ccb89065080379dcdfbf0ded5bd2d8
Harshmistry-197 <harshmistry072004@gmail.com>
2026-02-23 12:04:00 +0530

    Add : Implemented linear regression on diabities dataset to check scaling before or afetr the train test split

---
+++ 23022026-monday/Linear Regression/main.py
@@ -0,0 +1,24 @@
+from sklearn.datasets import load_diabetes
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+
+df = load_diabetes()
+
+X = df.data
+y = df.target
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+
+scaling = StandardScaler()
+X_train = scaling.fit_transform(X_train)
+X_test = scaling.transform(X_test)
+
+regression = LinearRegression()
+regression.fit(X_train,y_train)
+
+y_pred = regression.predict(X_test)
+
+r2 = r2_score(y_test,y_pred)
+print(r2)
