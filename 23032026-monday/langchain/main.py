59f597ac77e066727487f2d2cd156340a4cd9553
Harshmistry-197 <harshmistry072004@gmail.com>
2026-03-23 17:39:05 +0530

    Add : Used .env for base_url

--- 23032026-monday/langchain/main.py
+++ 23032026-monday/langchain/main.py
@@ -1,10 +1,16 @@
 from langchain_ollama import ChatOllama
 from langchain_core.messages import HumanMessage
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+base_url = os.getenv("BASE_URL")
 
 llm = ChatOllama(
     model="llama3.2:latest",
     temperature=0.7,
-    base_url="http://ai:11434",
+    base_url=base_url,
 )
 
 response = llm.invoke([
